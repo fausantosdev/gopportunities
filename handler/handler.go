34712f443684855452bd6a42d584ededfc033a42
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-07-14 13:55:49 +0000

    refactor: add separate files for route handlers

--- handler/handler.go
+++ handler/handler.go
@@ -1,31 +1,5 @@
 package handler
 
-import (
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
 
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "It's Ok",
-	})
-}
 
-func ReadOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "It's Ok",
-	})
-}
-
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "It's Ok",
-	})
-}
-
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "It's Ok",
-	})
-}
